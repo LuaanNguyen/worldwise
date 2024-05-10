a1b6d878378608ab108e4da830ef89ab5bf92766
ltnguy359 <153343039+ltnguy359@users.noreply.github.com>
2024-05-10 15:25:50 -0700

    Adding nav pages

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import styles from "./AppNav.module.css";
+
+function AppNav() {
+  return <div className={styles.nav}>App navigation</div>;
+}
+
+export default AppNav;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,10 @@
+import AppNav from "../components/AppNav";
+function AppLayout() {
+  return (
+    <div>
+      <AppNav /> <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
