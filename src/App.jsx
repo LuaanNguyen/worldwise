b62ad4ad9f1f33b21608a604c1822a2d949cb6dc
ltnguy359 <153343039+ltnguy359@users.noreply.github.com>
2024-05-11 12:45:23 -0700

    Fetching fake API with delay, display the data.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,31 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:8000/cities";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(BASE_URL);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +39,14 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           {/* Index Route: a route that can shows when there's no other route that matches */}
-          <Route index element={<p>List of Cities</p>}></Route>
-          <Route path="cities" element={<p>List of Cities</p>}></Route>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
           <Route path="countries" element={<p>List of Countries</p>}></Route>
           <Route path="form" element={<p>Form</p>}></Route>
         </Route>
