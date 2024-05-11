4c7b7de9c3fd093d5217a43bc77a72ae77f5f5c2
ltnguy359 <153343039+ltnguy359@users.noreply.github.com>
2024-05-10 17:35:13 -0700

    updated static layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WordWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
