2cb2fa59d1364d84093e6bf5396d0883d49f664c
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-13 20:44:57 -0700

    Added Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -11,57 +9,29 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-
-const BASE_URL = "http://localhost:8000/cities";
+import { CitiesProvider } from "./context/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(BASE_URL);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />}>
-          {/* Index Route: a route that can shows when there's no other route that matches */}
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          ></Route>
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          ></Route>
-          <Route path="cities/:id" element={<City />}></Route>
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          ></Route>
-          <Route path="form" element={<Form />}></Route>
-        </Route>
-
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            {/* Index Route: a route that can shows when there's no other route that matches */}
+            <Route index element={<CityList />}></Route>
+            <Route path="cities" element={<CityList />}></Route>
+            <Route path="cities/:id" element={<City />}></Route>
+            <Route path="countries" element={<CountryList />}></Route>
+            <Route path="form" element={<Form />}></Route>
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
